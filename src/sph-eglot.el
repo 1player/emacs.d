9303e542651d9f994598996eaf39afa7082feccf
Stéphane Travostino <sph@combo.cc>
2023-03-03 10:24:03 +0000

    Disable eglot/flymake clickable errors

--- src/sph-eglot.el
+++ src/sph-eglot.el
@@ -3,6 +3,11 @@
   :defer nil
   :hook (((go-mode php-mode python-mode rust-mode typescript-mode) . eglot-ensure))
   :init
+  (add-hook 'eglot-managed-mode-hook
+            (lambda ()
+              (put 'eglot-note 'flymake-overlay-control nil)
+              (put 'eglot-warning 'flymake-overlay-control nil)
+              (put 'eglot-error 'flymake-overlay-control nil)))
   (setq eglot-ignored-server-capabilities '(:documentHighlightProvider)))
 
 (defun eglot-format-buffer-on-save ()
